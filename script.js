5ad2da4bdb43eaae497a45d77c110b281816e80b
Renan Rodrigo <renanrb@ime.usp.br>
2018-08-24 12:04:21 -0300

    Definir a logica para o fim do jogo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function aleatorio(min, max){
+﻿function aleatorio(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
@@ -6,8 +6,26 @@ let quadradinhos = document.querySelectorAll('.quadradinho');
 
 const cores = ['yellow', 'blue', 'red', 'green'];
 
+// Função foi criada com nome de 'bg' para tornar o código mais legível.
+function bg(quadradinho){
+    return quadradinho.style.backgroundColor;
+}
+
+function encerrarJogo(){
+    for(quadradinho of quadradinhos){
+        quadradinho.onmouseenter = null;
+    }
+    alert("Parabéns, fim de jogo!");
+}
+
 for(quadradinho of quadradinhos){
     quadradinho.onmouseenter = function(){
         this.style.backgroundColor = cores[aleatorio(0,3)];
+
+        if(bg(quadradinhos[0]) === bg(quadradinhos[1]) &&
+           bg(quadradinhos[1]) === bg(quadradinhos[2]) &&
+           bg(quadradinhos[2]) === bg(quadradinhos[3])){
+                encerrarJogo();
+           }
     }
 }
